bug in coverage normalization and integer overflow fixed

nanocaller_src/generate_SNP_pileups.py
@@ -161,7 +161,7 @@ def get_snp_testing_candidates(dct, region):
     pileup_list=[]
         
     pos_list=output.keys()
-
+    current_depth=[]
     output_pos,output_ref,output_mat,output_dp,output_freq=[],[],[],[],[]
     if pos_list:
 
@@ -214,22 +214,23 @@ def get_snp_testing_candidates(dct, region):
             total_ref=total_ref[np.newaxis,:]
             mat=np.dstack([mat,np.zeros([4,mat.shape[1]])+np.eye(4)[base_to_num_map[ref_dict[v_pos]]][:,np.newaxis]])
             data=np.vstack([total_ref,np.multiply(mat,1-2*total_ref)])
-            data=np.hstack([np.zeros([5,nbr_size-len(ls_total_1),5]),data,np.zeros([5, nbr_size-len(ls_total_2), 5])]).astype(np.int8)
+            data=np.hstack([np.zeros([5,nbr_size-len(ls_total_1),5]),data,np.zeros([5, nbr_size-len(ls_total_2), 5])]).astype(np.int32)
             
             output_pos.append(v_pos)
             output_ref.append(base_to_num_map[ref_dict[v_pos]])
             output_mat.append(data)
             output_dp.append(output[v_pos][0])
             output_freq.append(output[v_pos][1])
+            current_depth.append(len(sample))
             
         if len(output_pos)>0:
             output_mat=np.array(output_mat).astype(np.float32)    
             output_pos=np.array(output_pos)
 
-            output_ref=np.eye(max(4,np.max(output_ref)+1))[np.array(output_ref)].astype(np.int8)
+            output_ref=np.eye(max(4,np.max(output_ref)+1))[np.array(output_ref)].astype(np.int32)
             output_ref=output_ref[:,:4]
 
             output_dp=np.array(output_dp)
             output_freq=np.array(output_freq)
     
-    return (output_pos,output_ref,output_mat,output_dp,output_freq)
+    return (output_pos,output_ref,output_mat,output_dp,output_freq,np.mean(current_depth))

nanocaller_src/snpCaller.py
@@ -76,13 +76,11 @@ def caller(params, chunks_Q, counter_Q, snp_files):
         while not chunks_Q.empty():
             chunk = chunks_Q.get(block=False)
             chrom=chunk['chrom']
-            pos, test_ref, x_test, dp, freq = get_snp_testing_candidates(params, chunk)
+            pos, test_ref, x_test, dp, freq, coverage = get_snp_testing_candidates(params, chunk)
             
             if len(pos)>0:
                 test_ref=test_ref.astype(np.float16)
                 x_test=x_test.astype(np.float32)
-
-                coverage=np.mean(dp)
 
                 x_test[:,1:,:,:4]=x_test[:,1:,:,:4]*(train_coverage/coverage)
 
@@ -95,8 +93,6 @@ def caller(params, chunks_Q, counter_Q, snp_files):
                     batch_ref = test_ref[batch*batch_size:min((batch+1)*batch_size, len(test_ref))]
 
                     batch_coverage=np.mean(batch_dp)
-
-                    batch_x[:,1:,:,:4]=batch_x[:,1:,:,:4]*(train_coverage/batch_coverage)
 
                     batch_prob_A, batch_prob_G, batch_prob_T, batch_prob_C, batch_prob_GT = snp_model([batch_x, batch_ref[:,0][:,np.newaxis], batch_ref[:,1][:,np.newaxis], batch_ref[:,2][:,np.newaxis], batch_ref[:,3][:,np.newaxis]])
 